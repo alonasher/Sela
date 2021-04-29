06a4037990e690944ef76a5e21e78190e5bdc12c
Alon Asher <Alonasher9@gmail.com>
2021-04-29 11:44:25 +0300

    first commit

---
+++ Controllers/FlightsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Dal;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    [ApiController]
+    [Route("api/flights")]
+    public class FlightsController : Controller
+    {
+        private FlightsContext _context;
+        public FlightsController(FlightsContext context)
+        {
+            _context = context;
+        }
+        //GET api/flights
+        [HttpGet]
+        public IEnumerable<Flight> Get()
+        {
+            return _context.Flights.ToList();
+        }
+
+        // POST api/flights
+        [HttpPost]
+        public IActionResult Post([FromBody] Flight flight)
+        {
+            _context.Flights.Add(flight);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(Get), flight.Id);
+        }
+
+    }
+}
---
+++ Dal/FlightsContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication3.Models;
+
+namespace WebApplication3.Dal
+{
+    public class FlightsContext:DbContext
+    {
+        public FlightsContext(DbContextOptions<FlightsContext> options): base(options)
+        {
+        }
+
+        public DbSet<Flight> Flights { get; set; }
+    }
+}
